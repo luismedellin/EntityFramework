b5b7604d2d364a6187eccdeec80ea27850830c6b
luismedellin <luiseduardo1218@gmail.com>
2017-07-12 22:43:59 -0500

    Pluralsight 1

---
+++ PluralsightEF.DataModel/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace PluralsightEF.DataModel
+{
+    public class Class1:DbContext
+    {
+        public DbSet<Ninja> Ninjas { get; set; }
+        public DbSet<Clan> Clans{ get; set; }
+        public DbSet<NinjaEquipment> Equipments { get; set; }
+    }
+}
